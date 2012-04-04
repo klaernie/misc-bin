ab414769fc87e62f6a134a67eaad2cc31ed076bb
Andre Klärner <kandre@ak-online.be>
2012-04-04 20:25:17 +0200

    add a check to determine if to connect via vpn or not

--- bin/run-mutt.sh
+++ bin/run-mutt.sh
@@ -3,5 +3,10 @@ if [[ "`hostname`" = "mainframe" ]]
 then
 	gnome-terminal -t mutt -e mutt
 else
-	gnome-terminal -t mutt -e "ssh mainframe-vpn -t screen mutt"
+	if host vpn.ak-online.be 2>&1 >/dev/null 
+	then
+		gnome-terminal -t mutt -e "ssh mainframe-vpn -t screen mutt"
+	else
+		gnome-terminal -t mutt -e "ssh mainframe -t screen mutt"
+	fi
 fi
