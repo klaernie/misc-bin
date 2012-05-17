48499b6c7f2aea3b09885a11cbdff0585acb25d9
Andre Klärner <kandre@ak-online.be>
2012-05-17 15:59:58 +0200

    add zsh -i to running mutt for S06_aliases
    in S06_exports is $COLUMNS set and exported. This way the elinks from
    .mailcap can use $COLUMNS to define the dump-width, at which it has to
    dump the content

--- bin/run-mutt.sh
+++ bin/run-mutt.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -x
 if [[ "`hostname`" = "mainframe" ]]
 then
-	gnome-terminal -t mutt -e mutt
+	gnome-terminal -t mutt -e '/bin/zsh -i -c mutt'
 else
 	if host vpn.ak-online.be 2>&1 >/dev/null 
 	then
-		gnome-terminal -t mutt -e "ssh mainframe-vpn -t screen mutt"
+		gnome-terminal -t mutt -e "ssh mainframe-vpn -t screen /bin/zsh -i -c mutt"
 	else
-		gnome-terminal -t mutt -e "ssh mainframe -t screen mutt"
+		gnome-terminal -t mutt -e "ssh mainframe -t screen /bin/zsh -i -c mutt"
 	fi
 fi
