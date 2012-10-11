528eb2162701be407ca8547917fd33554004a19a
Andre Klaerner (C5147403) <andre.klaerner@sap.com>
2012-10-11 23:41:01 +0200

    replace run-mutt.sh with a more general version

---
+++ bin/run-app.sh
@@ -0,0 +1,55 @@
+#!/bin/bash -x
+
+if [[ "$0" =~ run-([a-z]+).sh ]]
+then 
+	APP=${BASH_REMATCH[1]}
+else
+	APP=mutt
+fi
+
+
+ssh=true
+case "`hostname`" in
+	mainframe)
+			vpn=false
+			ssh=false
+		;;
+	eliza)
+			if host vpn.ak-online.be 2>&1 >/dev/null
+			then
+				vpn=true
+			else
+				vpn=false
+			fi
+		;;
+	sapdeb2|mia)
+			vpn=true
+		;;
+esac
+
+xterm=$(readlink /etc/alternatives/x-terminal-emulator)
+case "$xterm" in
+ /usr/bin/urxvt)
+	exec_line="urxvt -title $APP -e"
+	;;
+ /usr/bin/gnome-terminal.wrapper)
+	exec_line="gnome-terminal -t $APP -x"
+	;;
+esac
+
+if [[ -z "$DISPLAY" ]]
+then
+	unset exec_line
+fi
+
+if [[ "$vpn" = "true" ]]
+then
+	$exec_line ssh mainframe-vpn -t "screen -c .screenrc.$APP -S $APP -dRR"
+else
+	if [[ "$ssh" = "true" ]]
+	then
+		$exec_line ssh mainframe -t "screen -c .screenrc.$APP -S $APP -dRR"
+	else
+		$exec_line screen -c .screenrc.$APP -S $APP -dRR
+	fi
+fi
