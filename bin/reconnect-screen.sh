b204791a2f678a480b9a65f445e0d7c4f99018c7
Andre Klaerner <andre.klaerner@sap.com>
2016-02-10 12:24:30 +0100

    improve reconnect-screen messages

--- bin/reconnect-screen.sh
+++ bin/reconnect-screen.sh
@@ -6,10 +6,14 @@ if [[ $# -ne 1 ]]; then
 fi
 
 if [[ -n "$DISPLAY" ]]; then
+	echo "setting \$DISPLAY to $DISPLAY"
 	screen -S "$1" -X setenv DISPLAY "$DISPLAY"
+	echo "done"
 fi
 if [[ -n "$SSH_AUTH_SOCK" ]]; then
+	echo "setting \$SSH_AUTH_SOCK to $SSH_AUTH_SOCK"
 	screen -S "$1" -X setenv SSH_AUTH_SOCK "$SSH_AUTH_SOCK"
+	echo "done"
 fi
 
 exec screen -RD -S "$1"
